70093045947c974b38d83010409d43b3130e0110
Joshua Betrand <Joshua.betrand@gmail.com>
2022-06-29 10:23:41 -0400

    Added index.html file with new image and hover effect

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let response =('https://api.adviceslip.com/advice');
 console.log(response);
 
-function AdiviceofDay(){
     $.ajax('https://api.adviceslip.com/advice',
     {
         success: function (APIResponse) {
-            // console.log(APIResponse);
+            console.log(APIResponse);
+            let res = JSON.parse(APIResponse);
+            console.log(res);
             let myDiv = document.createElement('div');
 
             // Creating a paragraph element and adding the innerHTML
@@ -14,7 +15,7 @@ function AdiviceofDay(){
 
             // Creating another paragraph element and adding the innerHTML
             let adviceoftheDay = document.createElement('p');
-            adviceoftheDay.innerHTML = APIResponse.advice;
+            adviceoftheDay.innerHTML = res.slip.advice;
 
             // Appending the paragraph tags to the created div element
             myDiv.appendChild(advicetitle);
@@ -23,26 +24,4 @@ function AdiviceofDay(){
             document.body.appendChild(myDiv);
         }
     })
-}
-
-
-    // function solarresults(){
-    //     $.ajax(myURL,
-    //     {
-    //         success: function (APIResponse) {
-    //             console.log(APIResponse);
-    //              let myDiv = document.createElement('div');
-    
-    //             let myResponselat = document.createElement('p');
-    //             myResponselat.innerHTML = "Sunrise Time: " + APIResponse.results.sunrise;
-    
-    //             let myResponselng = document.createElement('p');
-    //             myResponselng.innerHTML = "Sunset Time: " + APIResponse.results.sunset;
-    
-    //             myDiv.appendChild(myResponselat);
-    //             myDiv.appendChild(myResponselng);
-    
-    //             document.body.appendChild(myDiv);
-    //         }
-    //     })
-    // }
+   
